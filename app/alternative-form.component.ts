4844a84a4fba404fd0ff670b68c19af645b6e5e2
Daniel Nieto <danielnieto89@gmail.com>
2016-07-20 15:35:46 -0500

    implement new component to test an alternative way of using forms

---
+++ app/alternative-form.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "alternative-form",
+    templateUrl: "./app/alternative-form.component.html"
+})
+export class AlternativeFormComponent{
+
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,16 @@
 //our root app component
 import {Component} from '@angular/core'
 import {HeroFormComponent} from "./hero-form.component";
+import {AlternativeFormComponent} from "./alternative-form.component";
 
 @Component({
   selector: 'my-app',
   providers: [],
   template: `
-      <hero-form>loading form</hero-form>
+     <!-- <hero-form>loading form</hero-form> -->
+      <alternative-form>Loading alternative form</alternative-form>
   `,
-  directives: [HeroFormComponent]
+  directives: [HeroFormComponent, AlternativeFormComponent]
 })
 export class AppComponent {
 

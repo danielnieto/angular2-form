ebc840e42b82e40db05b596a5478ec1b84722202
Daniel Nieto <danielnieto89@gmail.com>
2016-07-21 00:23:31 -0500

    form with FormBuilder working and using custom validators

--- app/alternative-form.component.ts
+++ app/alternative-form.component.ts
@@ -1,9 +1,45 @@
 import {Component} from "@angular/core";
+import {
+  FORM_DIRECTIVES,
+  REACTIVE_FORM_DIRECTIVES,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl
+} from '@angular/forms';
 
 @Component({
     selector: "alternative-form",
-    templateUrl: "./app/alternative-form.component.html"
+    templateUrl: "./app/alternative-form.component.html",
+    directives: [FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES]
 })
 export class AlternativeFormComponent{
+
+    myForm: FormGroup;
+    sku: AbstractControl;
+
+    onSubmit(){
+        alert(JSON.stringify(this.myForm.value));
+    }
+
+    constructor(private fb: FormBuilder){
+        this.myForm = fb.group({
+            'sku':["", Validators.compose([Validators.required, skuValidator])],
+            'name':["no tiene"]
+        });
+
+         this.sku = this.myForm.controls['sku'];
+    }
+
+
+    get diagnostics(){
+        return JSON.stringify(this.myForm.value);
+    }
+
+    function skuValidator(control: FormControl): { [s: string]: boolean } {
+      if (!control.value.match(/^123/)) {
+        return {invalidSku: true};
+      }
+    }
 
 }
